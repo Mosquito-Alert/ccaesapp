a7d5a247e925d6062373f4d1b26363a7e29da8c7
aescobarr <agustiescobar@gmail.com>
2023-06-01 16:28:49 +0200

    Adjust settings for ci

--- ccaesapp/settings.py
+++ ccaesapp/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'ccaesapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -126,4 +138,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = "/"
-from ccaesapp.settings_local import *
+
+if not os.getenv('GITHUB_WORKFLOW'):
+    from ccaesapp.settings_local import *
